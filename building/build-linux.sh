06979e51a925d829b4373a23d78dd146839c2872
Markus Schwenk <kontakt@markus-schwenk.de>
2019-04-03 20:54:33 +0200

    Correctly report build failures

--- building/build-linux.sh
+++ building/build-linux.sh
@@ -2,3 +2,10 @@
 qmake -qt=qt5 -project -v
 qmake -qt=qt5 midieditor.pro
 make
+status=$?
+if [ $status -eq 0 ]; then
+echo "build was successful"
+else
+echo "build failed"
+exit 1
+fi
--- building/build-windows.sh
+++ building/build-windows.sh
@@ -9,5 +9,12 @@ mkdir .build
 cd .build
 
 $MXE/usr/i686-w64-mingw32.static/qt5/bin/qmake ../../ && make
+status=$?
+if [ $status -eq 0 ]; then
+echo "build was successful"
+else
+echo "build failed"
+exit 1
+fi
 
 cd ../../
