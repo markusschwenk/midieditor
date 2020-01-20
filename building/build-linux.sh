f9458c3537310e0606a93e5dac6e6305f8dc06e6
Atikur Rahman Chitholian <chitholian@gmail.com>
2020-01-21 00:17:27 +0600

    Fixed: qmake error: '***Unknown option -qt=qt5' 	modified:   building/build-linux.sh 	modified:   src/midi/Metronome.cpp 1. In Fedora 31 (KDE), qmake error: '***Unknown option -qt=qt5' during build-process is fixed.
    2. GStreamer; Unable to pause - "file:///.../metronome/metronome-01.wav" was fixed.

--- building/build-linux.sh
+++ building/build-linux.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-qmake -qt=qt5 -project -v
-qmake -qt=qt5 midieditor.pro
+QMAKE=`which qmake-qt5 2> /dev/null`
+QMAKE_ARGS=""
+if [ ! -x "$QMAKE" ];then
+    QMAKE=qmake
+    QMAKE_ARGS="-qt=qt5"
+fi
+"$QMAKE" $QMAKE_ARGS -project -v
+"$QMAKE" $QMAKE_ARGS midieditor.pro
 make
 status=$?
 if [ $status -eq 0 ]; then
